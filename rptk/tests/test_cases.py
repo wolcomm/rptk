ab955ea85695548ceb54cc465dc01fc9f43e7cec
Ben Maddison <benm@workonline.co.za>
2016-11-21 09:42:32 +0200

    new test cases

--- rptk/tests/test_cases.py
+++ rptk/tests/test_cases.py
@@ -1,11 +1,25 @@
 import os
 from unittest import TestCase
 from whichcraft import which
+from rptk import configuration, dispatch
 
 
-class TestOutput(TestCase):
+class TestRptk(TestCase):
     config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tests.conf')
+    argv = ['--config_path', config_path]
 
-    def test_01_dependency(self):
+    def test_01_dependency_bgpq3(self):
         path = which('bgpq3')
         self.assertIsInstance(path, str, msg="bgpq3 executable not found in PATH")
+
+    def test_02_configure(self):
+        config = configuration.Config(argv=self.argv)
+        self.assertIsInstance(config, configuration.Config, msg="config object invalid")
+        self.config = config
+
+    def test_03_dummy_query(self):
+        dispatcher = dispatch.Dispatcher(config=self.config)
+        result = dispatcher.dispatch()
+        self.assertIsInstance(result, dict)
+        self.assertIsInstance(result["test"], dict)
+        self.assertEqual(result["test"]["object"], self.config.args.object)

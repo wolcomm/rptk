13bec1ed017e616470c49960f18ab26048c11740
Ben Maddison <benm@workonline.co.za>
2016-11-26 12:09:16 +0200

    exception catching in command_line

--- rptk/command_line.py
+++ rptk/command_line.py
@@ -4,24 +4,32 @@ from rptk import configuration, dispatch
 
 
 def main():
-    lh = logging.StreamHandler()
-    lf = logging.Formatter(fmt="%(asctime)s %(name)s: %(levelname)s %(message)s")
-    lh.setFormatter(lf)
-    logging.getLogger().addHandler(lh)
-    logging.getLogger().setLevel(logging.DEBUG)
     log = logging.getLogger(__name__)
-    log.debug(msg="started")
-    argv = sys.argv[1:]
-    log.debug(msg="have %s arguments" % len(argv))
-    config = configuration.Config(argv=argv)
-    log.debug(msg="configuration done")
-    dispatcher = dispatch.Dispatcher(config=config)
-    log.debug(msg="dispatcher ready")
-    result = dispatcher.dispatch()
-    log.debug(msg="got result length %s" % len(result))
-    sys.stdout.write("%s\n" % result)
-    return
-
+    try:
+        lh = logging.StreamHandler()
+        lf = logging.Formatter(fmt="%(asctime)s %(name)s: %(levelname)s %(message)s")
+        lh.setFormatter(lf)
+        logging.getLogger().addHandler(lh)
+        logging.getLogger().setLevel(logging.DEBUG)
+    except Exception as e:
+        log.error(msg=e.message)
+        return 1
+    try:
+        log.debug(msg="started")
+        argv = sys.argv[1:]
+        log.debug(msg="have %s arguments" % len(argv))
+        config = configuration.Config(argv=argv)
+        log.debug(msg="configuration done")
+        dispatcher = dispatch.Dispatcher(config=config)
+        log.debug(msg="dispatcher ready")
+        result = dispatcher.dispatch()
+        log.debug(msg="got result length %s" % len(result))
+        sys.stdout.write("%s\n" % result)
+        return 0
+    except Exception as e:
+        log.error(msg=e.message)
+        return 1
 
 if __name__ == "__main__":
-    main()
+    exit_code = main()
+    exit(exit_code)

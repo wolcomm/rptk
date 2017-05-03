a42b274e425720493a5f77fdcecb622b00be9f1a
Ben Maddison <benm@workonline.co.za>
2017-05-03 13:37:33 +0200

    updated .gitignore and bgpq3 install script

--- install-bgpq3.sh
+++ install-bgpq3.sh
@@ -3,3 +3,4 @@ set -ex
 wget https://github.com/snar/bgpq3/archive/v0.1.32.tar.gz
 tar -xzvf v0.1.32.tar.gz
 cd bgpq3-0.1.32/ && ./configure --prefix=/usr && make && sudo make install
+rm v0.1.32.tar.gz

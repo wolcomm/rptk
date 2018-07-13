f24f3196841eb4ff08aeba2b45e3fa9b0780be80
Ben Maddison <benm@workonline.co.za>
2018-07-14 00:34:24 +0200

    test api config file parsing

--- test/test_api.py
+++ test/test_api.py
@@ -15,24 +15,28 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from helpers import (available_policies, default_format_classes,
-                     default_query_classes, objects)
+                     default_query_classes, objects, test_config_file)
 
 import pytest
+
+
+config_file_opts = (None, test_config_file())
 
 
 @pytest.mark.usefixtures("mock_query_classes")
 class TestAPI(object):
     """Test cases for rptk python API."""
 
+    @pytest.mark.parametrize("config_file", config_file_opts)
     @pytest.mark.parametrize("q", default_query_classes().keys())
     @pytest.mark.parametrize("f", default_format_classes().keys())
     @pytest.mark.parametrize("p", available_policies().keys())
     @pytest.mark.parametrize("objects", objects())
-    def test_api(self, q, f, p, objects):
+    def test_api(self, config_file, q, f, p, objects):
         """Test rptk python API."""
         from rptk import RptkAPI
-        with RptkAPI(query_class_name=q, format_class_name=f,
-                     query_policy=p) as api:
+        with RptkAPI(config_file=config_file, query_class_name=q,
+                     format_class_name=f, query_policy=p) as api:
             result = api.query(*objects)
             output = api.format(result=result)
         assert output
